f5f1eed0b9f6d01482af8705bb3c7976969a22f1
szyszoch <pyrun97531@gmail.com>
2025-09-26 22:01:37 +0200

    Add DatabaseHelper
    Singleton class used for managing connection with database and base
    operations. Uses Task model to ensure task data is valid.

---
+++ lib/database_helper.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_task_manager/task.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _db;
+
+  DatabaseHelper._init();
+
+  Future<Database> get _database async {
+    _db ??= await _openTaskManagerDB();
+    return _db!;
+  }
+
+  Future<Database> _openTaskManagerDB() async {
+    return await openDatabase(
+      join(await getDatabasesPath(), 'task_manager.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, deadline INTEGER, completed_at INTEGER)',
+        );
+      },
+      version: 1,
+    );
+  }
+
+  Future<void> close() async {
+    final db = _db;
+    if (db != null) {
+      await db.close();
+      _db = null;
+    }
+  }
+
+  Future<Task> createTask(Task task) async {
+    final Database db = await _database;
+    final int id = await db.insert(
+      'tasks',
+      task.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    return task.copyWith(id: id);
+  }
+
+  Future<List<Task>> allTasks() async {
+    final db = await _database;
+    final List<Map<String, Object?>> taskMaps = await db.query('tasks');
+    return Task.fromMaps(taskMaps);
+  }
+
+  Future<int> updateTask(Task task) async {
+    if (task.id == null) throw ArgumentError('Task ID cannot be null');
+    final db = await _database;
+    return db.update(
+      'tasks',
+      task.toMap(),
+      where: 'id = ?',
+      whereArgs: [task.id],
+    );
+  }
+
+  Future<int> deleteTask(int id) async {
+    final db = await _database;
+    return db.delete('tasks', where: 'id = ?', whereArgs: [id]);
+  }
+}
