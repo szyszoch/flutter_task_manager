f681f97dfb3f7f06c354b9f23c435be99ea3afef
szyszoch <pyrun97531@gmail.com>
2025-09-27 20:55:06 +0200

    Add popup with task details

---
+++ lib/task_details.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task_manager/task_deadline_text.dart';
+import 'package:intl/intl.dart';
+import 'task.dart';
+
+void showTaskDetailsDialog(BuildContext context, Task task) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text(task.title),
+      content: ConstrainedBox(
+        constraints: const BoxConstraints(maxHeight: 300),
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 8,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TaskDeadlineText(task: task),
+
+              Text(
+                'Termin do: ${DateFormat('dd.MM.yyyy HH:mm').format(task.deadline)}',
+                style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+
+              const SizedBox(height: 8),
+
+              if (task.description?.isNotEmpty ?? false)
+                Text(
+                  task.description!,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                )
+              else
+                Center(
+                  child: Text(
+                    'Brak opisu',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text('Zamknij'),
+        ),
+      ],
+    ),
+  );
+}
